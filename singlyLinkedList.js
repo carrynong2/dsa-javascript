36470d6f81ca271c7d790fbccb20f3cd7f1cabca
Monthon Nong <carrynong2@gmail.com>
2022-12-28 21:25:50 +0700

    update shift and unshift method

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -50,11 +50,31 @@ class SinglyLinkedList {
     }
     return current;
   }
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
-list.push("HELLO");
-list.push("YOU");
-console.log(list.pop());
-console.log(list.pop());
+list.unshift("HELLO");
+list.unshift("YOU");
 console.log(list);

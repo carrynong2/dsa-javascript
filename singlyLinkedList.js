2c0a4bbcabd3806337f9167e0ca7a858eb0f6c46
Monthon Nong <carrynong2@gmail.com>
2022-12-28 21:08:08 +0700

    add traverse and pop method

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -26,9 +26,35 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+  traverse() {
+    let current = this.head;
+    while (current) {
+      console.log(current.val);
+      current = current.next;
+    }
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 let list = new SinglyLinkedList();
 list.push("HELLO");
 list.push("YOU");
+console.log(list.pop());
+console.log(list.pop());
 console.log(list);

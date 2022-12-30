e8ac05a4dcfbbef9ec86f0359a96e027cb7bacc0
Monthon Nong <carrynong2@gmail.com>
2022-12-30 10:51:03 +0700

    update push and pop methoid

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -12,7 +12,39 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
 }
 
-let first = new Node(12);
-console.log(first);
+let list = new DoublyLinkedList();
+list.push(15);
+list.push(20);
+list.pop();
+console.log(list);

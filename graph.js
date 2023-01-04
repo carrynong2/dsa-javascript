fd068ad4d74bcf19648e4d91b864db66e0638d5a
Monthon Nong <carrynong2@gmail.com>
2023-01-04 14:39:31 +0700

    update DFS with recursive

--- graph.js
+++ graph.js
@@ -3,17 +3,19 @@ class Graph {
     this.adjacencyList = {};
   }
   addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) {
-      this.adjacencyList[vertex] = [];
-    }
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
   addEdge(v1, v2) {
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1);
   }
-  removeEdge(v1, v2) {
-    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
-    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
   }
   removeVertex(vertex) {
     while (this.adjacencyList[vertex].length) {
@@ -22,12 +24,47 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+    return result;
+  }
 }
 
 let g = new Graph();
-g.addVertex("Dallas");
-g.addVertex("Tokyo");
-g.addEdge("Dallas", "Tokyo");
-console.log(g);
-g.removeVertex("Dallas");
-console.log(g);
+
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+console.log(g.depthFirstRecursive("A"));
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
